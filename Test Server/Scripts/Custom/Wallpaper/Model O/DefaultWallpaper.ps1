3c7dbddb78455f453797c9f8dc7c7d2231ac6891
Ethan Schmidtke <epcschmidtke@outlook.com>
2022-03-03 13:58:23 -0600

    Moved default wallpaper/user images
    *Moved numerous files/scripts in preparation of new script to manage applying wallpapers/images

--- Test Server/Scripts/Custom/Wallpaper/Model O/DefaultWallpaper.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item "D:\Windows\Web\4K" -Force
-Rename-Item "D:\Windows\Web\Wallpaper\Windows\img0.jpg" "img1.jpg" -Force
-Copy-Item "\\SERVER-2\Test$\Scripts\Custom\Wallpaper\Model O\img0.png" "D:\Windows\Web\Wallpaper\Windows\img0.jpg" -Force
