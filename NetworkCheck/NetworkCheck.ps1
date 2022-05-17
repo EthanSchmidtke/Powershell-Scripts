c096939876f663d645d743e71b0d052d52ff5276
Ethan Schmidtke <epcschmidtke@outlook.com>
2022-05-17 10:03:39 -0500

    Update NetworkCheck.ps1
    Attempted to fix issue of USB network adapters not being detected on startup without a physical replug by disabling the power management settings for USB Root Hub devices - This did not work.

--- NetworkCheck/NetworkCheck.ps1
+++ NetworkCheck/NetworkCheck.ps1
@@ -13,6 +13,55 @@ $i = 0
 function Get-NetworkStatus {
 
     $status = Get-NetConnectionProfile -Name "amd.com"
+
+    if ($NULL -eq $status) {
+
+        Write-Host "No connection to the amd.com network detected by either WiFi or Ethernet"
+        Write-Host "Disabling Power Management for USB Root Hub devices and restarting system"
+
+        # Disabling the two USB Root Hub's power saving feature did not resolve the issue on
+        #my Lenovo T16 - Disabling power saving for ALL devices under 'Universal Serial Bus Controllers'
+        #in Device Manager to see if that resovles it. If it does, need to update script to accomodate it.
+        # Disabling power saving for all other devies under 'Universal Serial Bus Controllers' did not
+        #resolve the issue either. There are two devices, 'USB4 Root Device Router', that will not
+        #retain the changes made to them after a reboot. Meaning disabling their power saving
+        #setting does not work. These could be responsible though I am not sure.
+        # I disabled the power saving feature directly for the USB ethernet adapter driver under
+        #'Network Devices'. I don't expect this to work but it may just.
+        $hubs = Get-WmiObject Win32_USBHub
+        $powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
+        foreach ($p in $powerMgmt) {
+
+            $IN = $p.InstanceName.ToUpper()
+            foreach ($h in $hubs) {
+
+                $PNPDI = $h.PNPDeviceID
+                if ($IN -like "*$PNPDI*") {
+
+                    if ($p.enable -eq $False) {
+
+                        Write-Host "Power saving already disabled on USB Root Hub, script failed to fix issue."
+                        EXIT
+
+                    } elseif ($p.enable -eq $True) {
+
+                        Write-Host "Disabling power saving on USB Root Hub"
+                        $p.enable = $False
+                        $p.psbase.put()
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        Start-Sleep -Seconds 1
+        Restart-Computer -Force
+
+    }
+
     $wifi = Get-NetAdapter -Name "Wi-Fi*"
 
     switch ($wifi.Status) {
@@ -22,6 +71,7 @@ function Get-NetworkStatus {
             Write-Host "System connected via WiFi"
             Write-Host "Disabling WiFi"
             Disable-NetAdapter -Name $wifi.Name
+            Start-Sleep -Seconds 10
 
         }
 
@@ -57,7 +107,6 @@ function Get-NetworkStatus {
             } elseif ($i -eq 2) {
                 
                 Write-Host "Network adapter reset did not resolve issue. Restarting computer."
-                Start-Sleep -Seconds 5
                 Restart-Computer -Force
                 Start-Sleep -Seconds 60
                 Write-Host "Restart may have failed. Attempting again."
@@ -85,5 +134,8 @@ function Get-NetworkStatus {
     }
     
 }
+
+Write-Host "Giving Windows 60 seconds to 'situate' network devices"
+Start-Sleep -Seconds 60
 
 Get-NetworkStatus
