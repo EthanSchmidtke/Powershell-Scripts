cb3f2cff9ffca71fca8c7ea591d9d7152864a54f
Ethan Schmidtke <epcschmidtke@outlook.com>
2022-06-07 12:42:20 -0500

    Update UpdateApps.ps1
    Drastic overhaul - Script has *some* idea of what apps, if any, are updating/have updated and will continually check for updates until all are completed (As far as it is aware)

--- Ironside/UpdateApps.ps1
+++ Ironside/UpdateApps.ps1
@@ -1,18 +1,85 @@
-$namespaceName = "root\cimv2\mdm\dmmap"
-$className = "MDM_EnterpriseModernAppManagement_AppManagement01"
-$wmiObj = Get-WmiObject -Namespace $namespaceName -Class $className
-$result = $wmiObj.UpdateScanMethod()
+<#
+NEW - Goals
 
-Write-Host "Sleeping while Store updates are processed"
-Start-Sleep -Seconds 120
+(x) Open Windows store
+(x) Select 'Check For Updates'
+(x) Find a way programmatically to check status of updates
+(x) Select 'Check For Updates' until no further updates are available
 
-Get-Process -Name "WinStore.App", "RuntimeBroker", "Microsoft Store Background Task Host"
+In the future, I may be able to use Get-AppxPackageAutoUpdateSettings
 
-#Runs a second time for those pesky apps
-$namespaceName = "root\cimv2\mdm\dmmap"
-$className = "MDM_EnterpriseModernAppManagement_AppManagement01"
-$wmiObj = Get-WmiObject -Namespace $namespaceName -Class $className
-$result = $wmiObj.UpdateScanMethod()
+#>
 
-Write-Host "Sleeping while Store updates are processed"
-Start-Sleep -Seconds 120
+Add-Type -AssemblyName System.Windows.Forms
+
+function Get-MSStoreUpdates {
+
+    [System.Collections.ArrayList]$appsPreUpdate = Get-AppxPackage -AllUsers
+    Write-Host "Checking for Microsoft Store app updates"
+    Start-Process ms-windows-store://downloadsandupdates
+    Start-Sleep -Seconds 10
+    [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+    Start-Sleep -Seconds 300
+    [System.Collections.ArrayList]$appsPostUpdate = Get-AppxPackage -AllUsers
+
+    Write-Host "Checked for updates 5 minutes ago. Checking results."
+    Get-PendingUpdates
+    
+}
+
+function Get-PendingUpdates {
+
+    switch ($appsPostUpdate.Count -gt $appsPreUpdate.Count) {
+
+        "True" {
+
+            Write-Host "More apps have been added to the system. Checking for updates again in 5 minutes."
+            Start-Sleep -Seconds 300
+            Get-MSStoreUpdates
+
+        }
+
+        "False" {
+
+            Write-Host "No new apps have been added to the system. Checking app version numbers."
+            $count = 0
+            $updatesPending = 0
+            $appsPostUpdate | ForEach-Object {
+
+                if ($_.Version -gt $appsPreUpdate[$count].Version) {
+                    Write-Host $_.Name "updated from version" $appsPreUpdate[$count].Version "to version" $_.Version
+                    $updatesPending++
+                } else {
+                    Write-Host $_.Name "did not update"
+                }
+                $count++
+
+            }
+
+            switch ($updatesPending) {
+
+                "0" {
+
+                    Write-Host "No apps updated. Exiting store and script."
+                    Stop-Process -Name WinStore.App -Force
+                    EXIT 0
+
+                }
+
+                Default {
+
+                    Write-Host $updatesPending "apps updated. Checking for additional updates in 5 minutes."
+                    Start-Sleep -Seconds 300
+                    Get-MSStoreUpdates
+
+                }
+
+            }
+
+        }
+
+    }
+    
+}
+
+Get-MSStoreUpdates
